f4ace51e18864cfa485e731c704baa26ef159d2f
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2024-10-22 00:57:35 +0700

    Update DahoodAutoFarm.lua

--- DahoodAutoFarm.lua
+++ DahoodAutoFarm.lua
@@ -345,3 +345,54 @@ end
 end)
 
 
+_G.Ignore = {}
+_G.SendNotifications = false
+_G.Settings = {
+	Players = {
+		["Ignore Me"] = false,
+		["Ignore Others"] = false,
+		["Ignore Tools"] = false
+	},
+	Meshes = {
+		NoMesh = false,
+		NoTexture = false,
+		Destroy = false
+	},
+	Images = {
+		Invisible = true,
+		Destroy = false
+	},
+	Explosions = {
+		Smaller = true,
+		Invisible = false, -- Not for PVP games
+		Destroy = false -- Not for PVP games
+	},
+	Particles = {
+		Invisible = true,
+		Destroy = false
+	},
+	TextLabels = {
+		LowerQuality = true,
+		Invisible = false,
+		Destroy = false
+	},
+	MeshParts = {
+		LowerQuality = true,
+		Invisible = false,
+		NoTexture = false,
+		NoMesh = false,
+		Destroy = false
+	},
+	Other = {
+		["FPS Cap"] = 360, -- true to uncap
+		["No Camera Effects"] = true,
+		["No Clothes"] = true,
+		["Low Water Graphics"] = true,
+		["No Shadows"] = true,
+		["Low Rendering"] = true,
+		["Low Quality Parts"] = true,
+		["Low Quality Models"] = true,
+		["Reset Materials"] = true,
+	}
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/FPSBooster.lua"))()
